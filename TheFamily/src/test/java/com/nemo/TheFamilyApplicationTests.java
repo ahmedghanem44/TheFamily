1489f1fd216f747123535afeb4f2182f79aa61ff
Nemo <ahmedghanem44@yahoo.com>
2019-09-24 23:05:46 -0400

    Project creation

---
+++ TheFamily/src/test/java/com/nemo/TheFamilyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TheFamilyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
